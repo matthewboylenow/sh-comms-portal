11ef24786a9877a684a7ac91a05a39ba995f1653
Matthew Boyle <36798189+matthewboylenow@users.noreply.github.com>
2025-01-16 02:14:17 +0000

    Added force dynamic to admin page

--- sainthelen-portal/app/admin/page.tsx
+++ sainthelen-portal/app/admin/page.tsx
@@ -1,6 +1,8 @@
 // app/admin/page.tsx
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useSession, signIn, signOut } from 'next-auth/react';
 import { useState, useEffect } from 'react';
 
